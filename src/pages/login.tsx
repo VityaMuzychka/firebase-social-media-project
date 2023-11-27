a7183dab87e1989512b20932da8aa647096561ed
VityaMuzychka <mirrorvitya@gmail.com>
2023-11-27 13:46:16 +0200

    check authentication functionality

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,18 @@
+import {auth, provider} from '../config/firebase';
+import { signInWithPopup } from 'firebase/auth';
+
 export const Login = () => {
+
+   const signInWithGoogle = async () => {
+    const result = await signInWithPopup(auth, provider);
+    console.log(result);
+    
+   }
+
     return (
         <div>
-          <h1>Login Page</h1>
+          <p>Sign In With Google To Continue</p>
+          <button onClick={signInWithGoogle}>Sign In With Google</button>
         </div>
     )
 }
