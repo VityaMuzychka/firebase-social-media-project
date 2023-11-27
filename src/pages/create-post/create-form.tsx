12b240f921244bd9fc59d6daf1898a1789661719
VityaMuzychka <mirrorvitya@gmail.com>
2023-11-27 22:43:35 +0200

    added an interface and error messages

--- src/pages/create-post/create-form.tsx
+++ src/pages/create-post/create-form.tsx
@@ -2,26 +2,31 @@ import {useForm} from 'react-hook-form';
 import * as yup from 'yup';
 import {yupResolver} from '@hookform/resolvers/yup';
 
-export const CreateForm = () => {
+interface CreateFormData {
+    title: string;
+    description: string;
+}
 
+export const CreateForm = () => {
  const schema = yup.object().shape({
     title: yup.string().required('You must add a title.'),
-    description: yup.string().required('Add description')
+    description: yup.string().required('Add a description.')
  })
 
- const {register, handleSubmit} = useForm({
+ const {register, handleSubmit, formState: {errors}} = useForm({
     resolver: yupResolver(schema)
  })
 
- const onCreatePost = (data: any) => {
+ const onCreatePost = (data: CreateFormData) => {
   console.log(data);
-  
  }
 
     return (
         <form onSubmit={handleSubmit(onCreatePost)}>
          <input placeholder='Title...' {...register('title')} />
+          <p style={{color: 'red'}}>{errors.title?.message}</p>
          <textarea placeholder='Description...' {...register('description')} />
+          <p style={{color: 'red'}}>{errors.description?.message}</p>
          <input type='submit' />
         </form>
     )
