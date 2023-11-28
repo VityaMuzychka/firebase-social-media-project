b5d503db025f4282116c760f11ec0dc5545fc4e0
VityaMuzychka <mirrorvitya@gmail.com>
2023-11-28 18:41:19 +0200

    destructured data into small object

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,4 +1,19 @@
+import {getDocs, collection} from 'firebase/firestore';
+import { db } from '../config/firebase';
+import { useState } from 'react';
+
+
 export const Main = () => {
+ const [postsList, setPostsList] = useState(null);
+ const postsRef = collection(db, 'posts');
+
+ const getPosts = async () => {
+  const data = await getDocs(postsRef);
+  console.log(data.docs.map(doc => ({...doc.data(), id: doc.id})));
+ }
+
+ getPosts();
+
     return (
         <div>
           <p>Main Page</p>
